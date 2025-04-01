8e868e9504605ebdd048692594b9799e499e14da
LuisIslas <al222310478@gmail.com>
2025-03-31 22:42:11 -0600

    Implementacion de distancia

--- app.py
+++ app.py
@@ -1,17 +1,15 @@
-# app.py
 from flask import Flask
 from config import db, migrate
 from dotenv import load_dotenv
 import os
-from flask import Blueprint
 from flask_jwt_extended import JWTManager
 from flask_cors import CORS
 
-# Cargar las variables de entorno desde un archivo .env
+# Cargar variables de entorno
 load_dotenv()
 
-# Inicialización de la aplicación Flask
-app = Flask(__name__)
+# Inicializar Flask
+app = Flask(_name_)
 
 # Permitir solo el dominio específico
 CORS(app, resources={
@@ -21,33 +19,31 @@ CORS(app, resources={
     r"/distancia/*": {"origins": "https://main.d3gd2kcl7rhrjn.amplifyapp.com"}
 })
 
+# Configuración de la base de datos
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 # Configuración de JWT
-app.config['JWT_SECRET_KEY'] = 'Prueba dia 08-02-2025'
+app.config["JWT_SECRET_KEY"] = os.getenv("JWT_SECRET_KEY", "Prueba dia 08-02-2025")
 
-# Configuración de la base de datos
-app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
-# Inicializar SQLAlchemy, Flask-Migrate y JWTManager
+# Inicializar extensiones
 db.init_app(app)
 migrate.init_app(app, db)
 jwt = JWTManager(app)
 
 # Ruta de prueba
-@app.route('/')
+@app.route("/")
 def home():
-    return '¡Hola, mundo!'
+    return "¡Hola, mundo!"
 
-# Importar los Blueprints de usuarios y roles
+# Importar y registrar Blueprints
 from routes.rutas import usuario_bp, roles_bp, baston_bp, distancia_bp
 
-# Registrar los Blueprints
-app.register_blueprint(usuario_bp, url_prefix='/usuario')
-app.register_blueprint(roles_bp, url_prefix='/roles')
-app.register_blueprint(baston_bp, url_prefix='/bastones')
-app.register_blueprint(distancia_bp, url_prefix='/distancia')
+app.register_blueprint(usuario_bp, url_prefix="/usuario")
+app.register_blueprint(roles_bp, url_prefix="/roles")
+app.register_blueprint(baston_bp, url_prefix="/bastones")
+app.register_blueprint(distancia_bp, url_prefix="/distancia")
 
-# Correr la aplicación
-if __name__ == '__main__':
-    app.run(debug=True)
+# Ejecutar la aplicación
+if _name_ == "_main_":
+    app.run(host="0.0.0.0", port=5000, debug=True)# Escuchar en todas las interfaces
